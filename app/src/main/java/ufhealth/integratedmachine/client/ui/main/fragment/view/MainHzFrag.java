a36b802f7e6950e81cdb3e911945098259098912
yxl <1172246321@qq.com>
2018-10-14 18:36:06 +0800

    完善框架

--- app/src/main/java/ufhealth/integratedmachine/client/ui/main/fragment/view/MainHzFrag.java
+++ app/src/main/java/ufhealth/integratedmachine/client/ui/main/fragment/view/MainHzFrag.java
@@ -5,6 +5,11 @@ import android.content.Intent;
 import android.widget.TextView;
 import android.view.LayoutInflater;
 import ufhealth.integratedmachine.client.R;
+import android.support.v7.widget.RecyclerView;
+import android.support.v4.widget.NestedScrollView;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import ufhealth.integratedmachine.client.base.BaseAct;
 import ufhealth.integratedmachine.client.base.BaseFrag;
 import com.yuan.devlibrary._11___Widget.promptBox.BasePopupWindow;
@@ -15,12 +20,16 @@ import ufhealth.integratedmachine.client.ui.main.fragment.presenter.MainHzPresen
 
 public class MainHzFrag extends BaseFrag implements MainHzFrag_V,View.OnClickListener
 {
-    private TextView mMainhzfragBj;
-    private TextView mMainhzfragYj;
+    private XAxis mXAxis;
+    private YAxis mYAxis;
     private TextView mMainhzfragSj;
     private TextView mMainhzfragSwcz;
     private TextView mMainhzfragTscz;
+    private RecyclerView mRecyclerView;
+    private BarChart mMainhzfragBarchart;
+    private TextView mainhzfragBarchartText;
     private MainHzPresenter mMainHzPresenter;
+    private NestedScrollView mNestedScrollView;
 
     protected int setLayoutResID()
     {
@@ -32,11 +41,50 @@ public class MainHzFrag extends BaseFrag implements MainHzFrag_V,View.OnClickLis
         super.initWidgets(rootView);
         setTitleContent("汇总统计");
         setTitleBack(R.mipmap.usericon);
-        mMainhzfragBj = (TextView)rootView.findViewById(R.id.mainhzfrag_bj);
-        mMainhzfragYj = (TextView)rootView.findViewById(R.id.mainhzfrag_yj);
         mMainhzfragSj = (TextView)rootView.findViewById(R.id.mainhzfrag_sj);
         mMainhzfragSwcz = (TextView)rootView.findViewById(R.id.mainhzfrag_swcz);
         mMainhzfragTscz = (TextView)rootView.findViewById(R.id.mainhzfrag_tscz);
+        mMainhzfragBarchart = (BarChart)rootView.findViewById(R.id.mainhzfrag_barchart);
+        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.mainhzfrag_recyclerview);
+        mainhzfragBarchartText = (TextView)rootView.findViewById(R.id.mainhzfrag_barchart_text);
+        mNestedScrollView = (NestedScrollView)rootView.findViewById(R.id.mainhzfrag_nestedscrollview);
+
+        mMainhzfragBarchart.setDrawBarShadow(false);
+        mMainhzfragBarchart.setDrawValueAboveBar(true);
+        mMainhzfragBarchart.setMaxVisibleValueCount(72);
+        mMainhzfragBarchart.setDrawGridBackground(false);
+        mMainhzfragBarchart.getDescription().setEnabled(false);
+        mMainhzfragBarchart.getXAxis().setLabelRotationAngle(0);
+
+        mXAxis = mMainhzfragBarchart.getXAxis();
+        mXAxis.setLabelCount(7);
+        mXAxis.setGranularity(1f);
+        mXAxis.setAxisMaximum(6f);
+        mXAxis.setAxisMinimum(0.5f);
+        mXAxis.setDrawAxisLine(true);
+        mXAxis.setDrawGridLines(false);
+        mXAxis.setXOffset(0);
+        mXAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        mMainhzfragBarchart.getAxisLeft().setDrawAxisLine(false);
+
+        mYAxis = mMainhzfragBarchart.getAxisLeft();
+        //设置Y左边轴显示的值 label 数量
+        mYAxis.setLabelCount(8, false);
+        //设置值显示的位置，我们这里设置为显示在Y轴外面
+        mYAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        //设置Y轴 与值的空间空隙 这里设置30f意为30%空隙，默认是10%
+        mYAxis.setSpaceTop(30f);
+        //设置Y轴最小坐标和最大坐标
+        mYAxis.setAxisMinimum(0f);
+        mYAxis.setAxisMaximum(80f);
+        mYAxis.setAxisMinValue(0f);
+        //Y轴右边轴的设置，跟左边轴类似
+        YAxis rightAxis = mMainhzfragBarchart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setSpaceTop(30f);
+        rightAxis.setAxisMinimum(0f);
+        rightAxis.setAxisMaximum(80f);
     }
 
     protected void initDatas()
@@ -47,11 +95,10 @@ public class MainHzFrag extends BaseFrag implements MainHzFrag_V,View.OnClickLis
 
     protected void initLogic()
     {
-        mMainhzfragBj.setOnClickListener(this);
-        mMainhzfragYj.setOnClickListener(this);
         mMainhzfragSj.setOnClickListener(this);
         mMainhzfragSwcz.setOnClickListener(this);
         mMainhzfragTscz.setOnClickListener(this);
+        mainhzfragBarchartText.setOnClickListener(this);
     }
 
     public void onClick(View view)
@@ -59,14 +106,6 @@ public class MainHzFrag extends BaseFrag implements MainHzFrag_V,View.OnClickLis
         super.onClick(view);
         switch (view.getId())
         {
-            case R.id.mainhzfrag_bj:
-            {
-                break;
-            }
-            case R.id.mainhzfrag_yj:
-            {
-                break;
-            }
             case R.id.mainhzfrag_sj:
             {
                 break;
@@ -76,6 +115,10 @@ public class MainHzFrag extends BaseFrag implements MainHzFrag_V,View.OnClickLis
                 break;
             }
             case R.id.mainhzfrag_tscz:
+            {
+                break;
+            }
+            case R.id.mainhzfrag_barchart_text:
             {
                 break;
             }
