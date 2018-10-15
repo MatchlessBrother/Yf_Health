b622e5b7131345454b4d9ed7b2751bd0712a37e4
yxl <1172246321@qq.com>
2018-10-15 19:00:52 +0800

    修复问题

---
+++ app/src/main/java/ufhealth/integratedmachine/client/bean/main/JcType.java
@@ -0,0 +1,65 @@
+package ufhealth.integratedmachine.client.bean.main;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.contrarywind.interfaces.IPickerViewData;
+
+public class JcType implements IPickerViewData, Parcelable
+{
+    private int typeCode;
+    private String typeName;
+
+    public JcType(int typeCode, String typeName) {
+        this.typeCode = typeCode;
+        this.typeName = typeName;
+    }
+
+    public int getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(int typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getPickerViewText() {
+        return typeName;
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.typeCode);
+        dest.writeString(this.typeName);
+    }
+
+    public JcType() {
+    }
+
+    protected JcType(Parcel in) {
+        this.typeCode = in.readInt();
+        this.typeName = in.readString();
+    }
+
+    public static final Creator<JcType> CREATOR = new Creator<JcType>() {
+        @Override
+        public JcType createFromParcel(Parcel source) {
+            return new JcType(source);
+        }
+
+        @Override
+        public JcType[] newArray(int size) {
+            return new JcType[size];
+        }
+    };
+}
--- app/src/main/java/ufhealth/integratedmachine/client/ui/main/fragment/view/MainJcFrag.java
+++ app/src/main/java/ufhealth/integratedmachine/client/ui/main/fragment/view/MainJcFrag.java
@@ -1,15 +1,23 @@
 package ufhealth.integratedmachine.client.ui.main.fragment.view;
 
+import java.util.List;
 import android.view.View;
+import java.util.ArrayList;
 import android.content.Intent;
 import android.widget.TextView;
-import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.view.LayoutInflater;
 import ufhealth.integratedmachine.client.R;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v4.widget.SwipeRefreshLayout;
+import com.bigkoo.pickerview.view.OptionsPickerView;
 import ufhealth.integratedmachine.client.base.BaseAct;
 import ufhealth.integratedmachine.client.base.BaseFrag;
+import ufhealth.integratedmachine.client.bean.main.JcType;
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import ufhealth.integratedmachine.client.bean.main.JcStatus;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
 import com.yuan.devlibrary._11___Widget.promptBox.BasePopupWindow;
 import ufhealth.integratedmachine.client.ui.main.activity.view.SignInAct;
 import ufhealth.integratedmachine.client.ui.main.fragment.view_v.MainJcFrag_V;
@@ -18,13 +26,19 @@ import ufhealth.integratedmachine.client.ui.main.fragment.presenter.MainJcPresen
 
 public class MainJcFrag extends BaseFrag implements MainJcFrag_V,View.OnClickListener
 {
+    private List<JcType> mLsList;
     private TextView mainjcfragLx;
     private TextView mainjcfragZt;
     private TextView mainjcfragLs;
+    private List<JcStatus> mZtList;
     private LinearLayout mainjcfragLxAll;
     private LinearLayout mainjcfragZtAll;
     private RecyclerView mainjcfragRecycler;
     private MainJcPresenter mMainJcPresenter;
+    private int mCurrentSelectedLsItemOfIndex;
+    private int mCurrentSelectedZtItemOfIndex;
+    private OptionsPickerView mLsOptionsPickerView;
+    private OptionsPickerView mZtOptionsPickerView;
     private SwipeRefreshLayout mainjcfragSwiperefreshlayout;
 
     protected int setLayoutResID()
@@ -46,12 +60,75 @@ public class MainJcFrag extends BaseFrag implements MainJcFrag_V,View.OnClickLis
         mainjcfragZtAll = (LinearLayout)rootView.findViewById(R.id.mainjcfrag_zt_all);
         mainjcfragRecycler = (RecyclerView)rootView.findViewById(R.id.mainjcfrag_recycler);
         mainjcfragSwiperefreshlayout = (SwipeRefreshLayout)rootView.findViewById(R.id.mainjcfrag_swiperefreshlayout);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(mActivity,2);
+        gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+        mainjcfragRecycler.setLayoutManager(gridLayoutManager);
+        /******************************************************************************************/
+        /******************************************************************************************/
+        mLsOptionsPickerView = new OptionsPickerBuilder(mActivity, new OnOptionsSelectListener()
+        {
+            public void onOptionsSelect(int options1Index, int options2Index, int options3Index, View view)
+            {
+                mCurrentSelectedLsItemOfIndex  = options1Index;
+                mLsOptionsPickerView.setSelectOptions(mCurrentSelectedLsItemOfIndex);
+                mainjcfragLx.setText(mCurrentSelectedLsItemOfIndex > -1 && mCurrentSelectedLsItemOfIndex < mLsList.size() ? mLsList.get(mCurrentSelectedLsItemOfIndex).getTypeName().trim() : "");
+            }
+        }) .setTitleText("类型选择").setLabels("","","").setTitleSize(33)
+           .setSubmitText("确定") .setCancelText("取消")
+           .setSubCalSize(28).setContentTextSize(18)
+           .setBgColor(getResources().getColor(R.color.white))
+           .setTitleColor(getResources().getColor(R.color.white))
+           .setSubmitColor(getResources().getColor(R.color.white))
+           .setCancelColor(getResources().getColor(R.color.white))
+           .setBackgroundId(getResources().getColor(R.color.white))
+           .setTitleBgColor(getResources().getColor(R.color.colorPrimary))
+           .setDividerColor(getResources().getColor(R.color.default_font_gray))
+           .setOutSideCancelable(true).isRestoreItem(false).isCenterLabel(false)
+           .setCyclic(false,false,false).setSelectOptions(0, 0, 0).build();
+
+        mZtOptionsPickerView = new OptionsPickerBuilder(mActivity, new OnOptionsSelectListener()
+        {
+            public void onOptionsSelect(int options1Index, int options2Index, int options3Index, View view)
+            {
+                mCurrentSelectedZtItemOfIndex = options1Index;
+                mZtOptionsPickerView.setSelectOptions(mCurrentSelectedZtItemOfIndex);
+                mainjcfragZt.setText(mCurrentSelectedZtItemOfIndex > -1 && mCurrentSelectedZtItemOfIndex < mZtList.size() ? mZtList.get(mCurrentSelectedZtItemOfIndex).getStatusName().trim() : "");
+            }
+        }) .setTitleText("状态选择").setLabels("","","").setTitleSize(33)
+                .setSubmitText("确定") .setCancelText("取消")
+                .setSubCalSize(28).setContentTextSize(18)
+                .setBgColor(getResources().getColor(R.color.white))
+                .setTitleColor(getResources().getColor(R.color.white))
+                .setSubmitColor(getResources().getColor(R.color.white))
+                .setCancelColor(getResources().getColor(R.color.white))
+                .setBackgroundId(getResources().getColor(R.color.white))
+                .setTitleBgColor(getResources().getColor(R.color.colorPrimary))
+                .setDividerColor(getResources().getColor(R.color.default_font_gray))
+                .setOutSideCancelable(true).isRestoreItem(false).isCenterLabel(false)
+                .setCyclic(false,false,false).setSelectOptions(0, 0, 0).build();
     }
 
     protected void initDatas()
     {
         mMainJcPresenter = new MainJcPresenter();
         bindBaseMvpPresenter(mMainJcPresenter);
+        mLsList = new ArrayList<>();
+        mLsList.add(new JcType(1,"高危"));
+        mLsList.add(new JcType(2,"危险"));
+        mLsList.add(new JcType(3,"警告"));
+        mLsList.add(new JcType(4,"预警"));
+        mLsList.add(new JcType(5,"注意"));
+        mLsOptionsPickerView.setNPicker(mLsList,null,null);
+
+        mZtList = new ArrayList<>();
+        mZtList.add(new JcStatus(1,"未开始"));
+        mZtList.add(new JcStatus(2,"准备开始"));
+        mZtList.add(new JcStatus(3,"已开始"));
+        mZtList.add(new JcStatus(4,"进行中"));
+        mZtList.add(new JcStatus(5,"已完成"));
+        mZtList.add(new JcStatus(6,"准备结束"));
+        mZtList.add(new JcStatus(7,"已结束"));
+        mZtOptionsPickerView.setNPicker(mZtList,null,null);
     }
 
     protected void initLogic()
@@ -66,12 +143,26 @@ public class MainJcFrag extends BaseFrag implements MainJcFrag_V,View.OnClickLis
         super.onClick(view);
         switch (view.getId())
         {
-            case R.id.mainjcfrag_lx:
+            case R.id.mainjcfrag_lx_all:
             {
+                if(null != mLsList && mLsList.size() > 0)
+                {
+                    mLsOptionsPickerView.setSelectOptions(mCurrentSelectedLsItemOfIndex);
+                    mLsOptionsPickerView.show();
+                }
+                else
+                    showToast("没有可以选择的内容！");
                 break;
             }
-            case R.id.mainjcfrag_zt:
+            case R.id.mainjcfrag_zt_all:
             {
+                if(null != mZtList && mZtList.size() > 0)
+                {
+                    mZtOptionsPickerView.setSelectOptions(mCurrentSelectedZtItemOfIndex);
+                    mZtOptionsPickerView.show();
+                }
+                else
+                    showToast("没有可以选择的内容！");
                 break;
             }
             case R.id.mainjcfrag_ls:
